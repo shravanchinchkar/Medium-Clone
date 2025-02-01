aff2c40ae9c322743d492679e68907187e786485
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-02-01 16:00:56 +0530

    add the try catch in authMiddleWare and create the blog end points

--- backend/src/middleWare.ts
+++ backend/src/middleWare.ts
@@ -4,25 +4,36 @@ const app = new Hono<{
   Bindings: {
     JWT_SECRET: string;
   };
+  Variables: {
+    userId: string;
+  };
 }>();
 
 export async function authMiddleware(c: any, next: any) {
   console.log("Middleware called!");
-
-  const header = c.req.header("Authorization");
+  const header = c.req.header("Authorization"); //get the header from the user
   if (!header) {
+    //if the header is empty execute the following block
     c.status(401);
     return c.json({ error: "unauthorized" });
   }
 
-  //below line dose the follwoing=>  Bearer token =>["Bearer","token"]
-  const token = header.split(" ")[1]; // finalToken has the =>token
+  try {
+    //below line dose the follwoing=>  Bearer token =>["Bearer","token"]
+    const token = header.split(" ")[1]; // finalToken has the =>token
+    const verifiedToken = await verify(token, c.env.JWT_SECRET);
 
-  const verifiedToken = await verify(token, c.env.JWT_SECRET);
-  if (!verifiedToken.id) {
+    console.log("Verified Id is:", verifiedToken.id);
+
+    if (!verifiedToken.id) {
+      c.status(403);
+      c.json({ error: "unauthorized" });
+    } else {
+      c.set("userId", verifiedToken.id);
+      await next();
+    }
+  } catch (err) {
     c.status(403);
-    c.json({ error: "unauthorized" });
-  } else {
-    await next();
+    return c.json({ message: "unauthorized!" });
   }
 }
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,43 +1,130 @@
 import { Hono } from "hono";
 import { authMiddleware } from "../middleWare";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 export const blogRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     // JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
   };
 }>().basePath("/blog");
 
 //Following is the middleWare
 // it says that any request comming to /bolg/*  (* means any thing after /blog/) will first execute the middleware the only will pass to the actual handler
 
-// blogRouter.use("/*", async (c, next) => {
-//   const header = c.req.header("Authorization");
-//   if (!header) {
-//     c.status(401);
-//     return c.json({ error: "unauthorized" });
-//   }
+//middleware extract the userId and pass it down to the route Handler
+blogRouter.use("/*", authMiddleware);
 
-//   //below line dose the follwoing=>  Bearer token =>["Bearer","token"]
-//   const token = header.split(" ")[1]; // finalToken has the =>token
+//following route create the blog
 
-//   const verifiedToken = await verify(token, c.env.JWT_SECRET);
-//   if (!verifiedToken.id) {
-//     c.status(403);
-//     c.json({ error: "unauthorized" });
-//   } else {
-//     await next();
-//   }
-// });
+blogRouter.post("/", async (c) => {
+  console.log("In POST route for Blog!");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-blogRouter.use("/*",authMiddleware);
+  const body = await c.req.json();
+  const userId = c.get("userId");
+  console.log("userId is :", userId);
 
-blogRouter.post("/", (c) => c.text("Blog post!"));
-
-blogRouter.put("/", (c) => c.text("blog change!"));
-
-//Here the id is the Request Parameter not query parameters
-blogRouter.get("/:id", (c) => {
-  return c.text("UnderConstruction");
+  try {
+    const createdBlog = await prisma.blog.create({
+      data: {
+        authorId: userId,
+        title: body.title,
+        content: body.content,
+      },
+    });
+    return c.json({
+      message: "Blog Created!",
+      blog: createdBlog,
+    });
+  } catch (err) {
+    console.log(err);
+    c.status(403);
+    return c.json({ message: "Can't Upload the Blog!" });
+  }
 });
 
-blogRouter.get("/bulk", (c) => c.text("get all the blogs!"));
+//following route is used to update the blog
+
+blogRouter.put("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const updatedBlog = await prisma.blog.update({
+      where: {
+        id: body.id,
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+    return c.json({
+      message: "Blog Updated!",
+      updatedBlog: updatedBlog,
+    });
+  } catch (err) {
+    console.log(err);
+    c.status(403);
+    return c.json({
+      blogId: body.id,
+      message: "Can't Update the Blog!",
+    });
+  }
+});
+
+//following blog Return all the blogs on the dashboard!
+//Need To add pagination to this route
+blogRouter.get("/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const allBlogs = await prisma.blog.findMany({});
+    return c.json({
+      blogs: allBlogs,
+    });
+  } catch (err) {
+    c.status(411);
+    return c.json({
+      message: "Error while fetching the blogs!",
+    });
+  }
+});
+
+//Here the id is the Request Parameter not query parameters
+//following route is used to get a specific blog
+blogRouter.get("/:id", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+    const reqParams = c.req.param("id");
+//   const body = await c.req.json();
+
+  try {
+    const findBlog = await prisma.blog.findFirst({
+      where: {
+        id: reqParams,
+      },
+    });
+    return c.json({
+      message: "Blog returned!",
+      blogIs: findBlog,
+    });
+  } catch (err) {
+    console.log(err);
+    c.status(411);
+    return c.json({ message: "Blog not present!" });
+  }
+});
